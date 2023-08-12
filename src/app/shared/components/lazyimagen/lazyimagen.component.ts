a1f02225a08859c4708366c2ecb5984c3a6d4a73
SayraPa <98625428+Minori35@users.noreply.github.com>
2023-08-12 10:33:34 -0600

    Leader 2

--- src/app/shared/components/lazyimagen/lazyimagen.component.ts
+++ src/app/shared/components/lazyimagen/lazyimagen.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { NodeWithI18n } from '@angular/compiler';
+import { Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-lazyimagen',
   templateUrl: './lazyimagen.component.html',
   styleUrls: ['./lazyimagen.component.css']
 })
-export class LazyimagenComponent {
+export class LazyimagenComponent implements OnInit{
 
+  @Input()
+  public url!: string 
+
+  @Input()
+  public alt!: string 
+
+  public hasLoaded : boolean = false;
+
+  ngOnInit(): void {
+      if(!this.url) throw new Error("Url property is  required")
+  }
+
+  onLoadn(){
+    console.log('Image Loaded');
+    this.hasLoaded= true
+    
+  }
 }
